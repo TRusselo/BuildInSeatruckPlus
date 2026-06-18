acfd249e7c6b68c29f06d9adef1f413aefd62b9c
Tristyn <tristynrusselo@gmail.com>
2026-06-17 21:49:41 -0600

    feat: port SeaTruckSegmentHelper

---
+++ src/SeaTruckSegmentHelper.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace SeatruckJukebox
+{
+    internal static class SeaTruckSegmentHelper
+    {
+        public static bool isPlayerInSeaTruckSegment()
+        {
+            return Player.main != null && Player.main.currentInterior is SeaTruckSegment;
+        }
+
+        public static SeaTruckSegment getCurrentSeaTruckSegment()
+        {
+            return Player.main != null ? Player.main.currentInterior as SeaTruckSegment : null;
+        }
+
+        public static SeaTruckSegment getParentSeaTruckSegment(GameObject gameObject)
+        {
+            return gameObject.GetComponentInParent<SeaTruckSegment>();
+        }
+
+        public static SubRoot getCurrentSeaTruckSegmentSubRoot()
+        {
+            var seg = getCurrentSeaTruckSegment();
+            return seg == null ? null : seg.gameObject.GetComponent<SubRoot>();
+        }
+    }
+}
